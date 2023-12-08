f166d2d7314cdf652d83ce14aa038f7db0fc20bd
Lincoln Wallace <lincoln.wallace@canonical.com>
2023-12-08 09:19:27 -0300

    fix: Unexport reset const
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- src/log/log.go
+++ src/log/log.go
@@ -11,20 +11,20 @@ const (
 	Green  = "\033[32m"
 	Yellow = "\033[33m"
 	White  = "\033[97m"
-	Reset  = "\033[0m"
+	reset  = "\033[0m"
 )
 
 func Printf(color, format string, args ...interface{}) {
 	msg := fmt.Sprintf(format, args...)
-	log.Printf(color + msg + Reset)
+	log.Printf(color + msg + reset)
 }
 
 func Println(color string, args ...interface{}) {
-	msg := color + fmt.Sprint(args...) + Reset
+	msg := color + fmt.Sprint(args...) + reset
 	log.Println(msg)
 }
 
 func Fatalf(format string, args ...interface{}) {
-	msg := Red + fmt.Sprintf(format, args...) + Reset
+	msg := Red + fmt.Sprintf(format, args...) + reset
 	log.Fatalf(msg)
 }
