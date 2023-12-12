61f0bdfa289ef600c07746028dfe2babcd2f57d3
Lincoln Wallace V. Almeida <lincoln.wallace@canonical.com>
2023-12-12 10:44:49 -0300

    Re-purpose to report Matter snap results (#13)
    * Remove Launchpad query
    
    * Add Test column to distinguish build and test workflow results
    
    * Replace list of snaps with Matter snaps
    
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.SetStyle(table.StyleColoredBright)
-	t.AppendHeader(table.Row{"Name", "Channel", "Version", "Arch", "Rev", "Date", "Build"})
+	t.AppendHeader(table.Row{"Name", "Channel", "Version", "Arch", "Rev", "Date", "Build", "Test"})
 
 	t.SetColumnConfigs([]table.ColumnConfig{
 		{Number: 1, AutoMerge: true},
@@ -52,44 +52,14 @@ func main() {
 			logger.Fatalf("Error querying snap store: %s", err)
 		}
 
-		// launchpad
-		builds, err := queryLaunchpad(k)
-		if err != nil {
-			logger.Fatalf("Error querying launchpad: %s", err)
-		}
-		revisionBuildStatus := make(map[uint]string)
-		for _, v := range builds.Entries {
-			// Setting a check mark only if we find the successful build result for a given revision.
-			// Alternative scenarios include results that have no revision number because:
-			// - build or artifact upload has failed (an actual failure)
-			// - build is too old and not returned in the query
-			// - build or artifact upload is pending
-			if v.StoreUploadRevision != nil && v.BuildState == "Successfully built" {
-				revisionBuildStatus[*v.StoreUploadRevision] = "✅"
-			}
-		}
-
 		// github
 		runs, err := queryGithub(v.GithubRepo)
 		if err != nil {
 			logger.Fatalf("Error querying launchpad: %s", err)
 		}
-		var totalSnapRuns, failedSnapRuns uint
-		testIcon := "🔴"
-		for _, run := range runs.WorkflowRuns {
-			if run.Name == "Snap Testing" {
-				totalSnapRuns++
-			}
-			if run.Conclusion == "failure" {
-				failedSnapRuns++
-				logger.Errorf("🔴 %s (%s)", run.DisplayTitle, run.HTMLURL)
-			}
-		}
-		if totalSnapRuns == 0 { // something is not right
-			testIcon = "🟠"
-		} else if failedSnapRuns == 0 {
-			testIcon = "🟢"
-		}
+
+		build := computeWorkflowRunStats(runs, "Snap Builder")
+		test := computeWorkflowRunStats(runs, "Snap Testing")
 
 		// fill the table
 		for _, cm := range info.ChannelMap {
@@ -100,17 +70,35 @@ func main() {
 				cm.Channel.Architecture,
 				cm.Revision,
 				cm.Channel.ReleasedAt.Format(time.Stamp),
-				revisionBuildStatus[cm.Revision],
+				fmt.Sprintf("%d/%d", build.success, build.total),
+				fmt.Sprintf("%d/%d", test.success, test.total),
 			}, table.RowConfig{AutoMerge: true})
 		}
-		t.AppendRow(table.Row{
-			fmt.Sprintf("%s failed %d/%d", testIcon, failedSnapRuns, totalSnapRuns),
-			"", "", "", "", "", "",
-		}, table.RowConfig{AutoMerge: true})
+
 		t.AppendSeparator()
 	}
 
 	t.Render()
+}
+
+type workflowRunStats struct {
+	total, success uint
+}
+
+func computeWorkflowRunStats(run *workflowRuns, runName string) (s workflowRunStats) {
+	for _, r := range run.WorkflowRuns {
+		if r.Name != runName {
+			continue
+		}
+		s.total++
+		if r.Conclusion == "success" {
+			s.success++
+			// logger.Successf("🟢 %s (%s)", r.DisplayTitle, r.HTMLURL)
+		} else {
+			logger.Errorf("🔴 %s (%s)", r.DisplayTitle, r.HTMLURL)
+		}
+	}
+	return s
 }
 
 type snapInfo struct {
@@ -151,30 +139,7 @@ func querySnapStore(snapName string) (*snapInfo, error) {
 	return &info, nil
 }
 
-type builds struct {
-	Entries []struct {
-		StoreUploadRevision *uint `json:"store_upload_revision"`
-		BuildState          string
-	}
-}
-
-func queryLaunchpad(projectName string) (*builds, error) {
-	logger.Infoln("Querying Launchpad for:", projectName)
-	res, err := http.Get(fmt.Sprintf("https://api.launchpad.net/devel/~canonical-edgex/+snap/%s/builds?ws.size=10&direction=backwards&memo=0", projectName))
-	if err != nil {
-		return nil, err
-	}
-
-	var builds builds
-	err = json.NewDecoder(res.Body).Decode(&builds)
-	if err != nil {
-		return nil, err
-	}
-
-	return &builds, nil
-}
-
-type runs struct {
+type workflowRuns struct {
 	WorkflowRuns []struct {
 		Name         string
 		Conclusion   string
@@ -184,14 +149,14 @@ type runs struct {
 	Message string
 }
 
-func queryGithub(project string) (*runs, error) {
+func queryGithub(project string) (*workflowRuns, error) {
 	logger.Infoln("Querying Github workflow runs for:", project)
 	res, err := http.Get(fmt.Sprintf("https://api.github.com/repos/%s/actions/runs?per_page=10&event=pull_request", project))
 	if err != nil {
 		return nil, err
 	}
 
-	var r runs
+	var r workflowRuns
 	err = json.NewDecoder(res.Body).Decode(&r)
 	if err != nil {
 		return nil, err
